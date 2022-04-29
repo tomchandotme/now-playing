37269b26b6b442da482e12d8a60fe71b06a71064
tomchan <tomchan@instelar.com>
2022-04-29 11:13:26 +0800

    basic svg output #3: styled

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 **Now Playing** on Spotify
 
 <a href="https://spotify.tomchan.me/now-playing?open">
-    <img src="https://spotify.tomchan.me/now-playing" width="64" height="64" alt="Now Playing">
+    <img src="https://spotify.tomchan.me/now-playing" width="256" height="80" alt="Now Playing">
 </a>
--- components/Widget.tsx
+++ components/Widget.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+const WIDTH = 320;
+const HEIGHT = 80;
+const IMAGE_SIZE = 64;
+const PADDING = 8;
+
 type WidgetProps = {
-    width: number;
-    height: number;
     song: {
         track?: string;
         artists?: string;
@@ -14,20 +17,83 @@ type WidgetProps = {
     image?: string;
 };
 
-const Widget = ({ width, height, image }: WidgetProps) => {
+const Widget = ({ image, song }: WidgetProps) => {
     return (
         <svg
             fill="none"
-            width={width}
-            height={height}
-            viewBox={`0 0 ${width} ${height}`}
+            width={WIDTH}
+            height={HEIGHT}
+            viewBox={`0 0 ${WIDTH} ${HEIGHT}`}
             xmlns="http://www.w3.org/2000/svg"
         >
-            <image href={image ?? null} width={width} height={height} />
+            <filter id="shadow" colorInterpolationFilters="sRGB">
+                <feDropShadow
+                    dx="0"
+                    dy="1"
+                    stdDeviation="1"
+                    floodOpacity="0.5"
+                />
+                <feDropShadow
+                    dx="0"
+                    dy="0"
+                    stdDeviation="3"
+                    floodOpacity="0.5"
+                />
+            </filter>
+            <defs>
+                <style>
+                    {`
+                        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700&display=swap');
+
+                        .text {
+                            filter: url(#shadow);
+                        }
+                    `}
+                </style>
+            </defs>
+            <image
+                href={image ?? null}
+                width={IMAGE_SIZE}
+                height={IMAGE_SIZE}
+                x={PADDING}
+                y={PADDING}
+            />
+            {song.track && (
+                <g>
+                    <text
+                        className="text"
+                        x={IMAGE_SIZE + PADDING * 2}
+                        y={HEIGHT / 4}
+                        width={WIDTH - IMAGE_SIZE - PADDING * 2}
+                        height={HEIGHT / 2}
+                        alignmentBaseline="central"
+                        fontFamily="Noto Sans TC"
+                        fontWeight={700}
+                        fontSize={16}
+                        fill="#fff"
+                    >
+                        {song.track}
+                    </text>
+                    <text
+                        className="text"
+                        x={IMAGE_SIZE + PADDING * 2}
+                        y={(HEIGHT * 3) / 4}
+                        width={WIDTH - IMAGE_SIZE - PADDING * 2}
+                        height={HEIGHT / 2}
+                        alignmentBaseline="central"
+                        fontFamily="Noto Sans TC"
+                        fontWeight={400}
+                        fontSize={14}
+                        fill="#fff"
+                    >
+                        {`${song.artists} - ${song.album}`}
+                    </text>
+                </g>
+            )}
             {/* <foreignObject>
                 <div
                     {...{ xmlns: 'http://www.w3.org/1999/xhtml' }}
-                    style={{ width: 64, height: 64, display: 'flex' }}
+                    style={{ width: IMAGE_SIZE, height: IMAGE_SIZE, display: 'flex' }}
                 >
                     <style>{`
                     * {
@@ -35,7 +101,7 @@ const Widget = ({ width, height, image }: WidgetProps) => {
                         box-sizing: border-box;
                     }
                     `}</style>
-                    <img src={image ?? null} width={64} height={64} />
+                    <img src={image ?? null} width={IMAGE_SIZE} height={IMAGE_SIZE} />
                 </div>
             </foreignObject> */}
         </svg>
